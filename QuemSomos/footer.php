e361abc839b964c3e97e181712fbaa1181833c5a
Thiago <thiago.ferreira164@etec.sp.gov.br>
2024-10-25 14:48:17 -0300

    aidicionei a pagina 'quem somos' com a estrutura

---
+++ QuemSomos/footer.php
@@ -0,0 +1,13 @@
+
+<footer>
+    <h4>contato</h4>
+    <ul>
+        <li>
+            <p>Telefone: 23423423426</p>
+        </li>
+    </ul>
+    <address>Thiago Caetano</address>
+    <address>Henrique Veiga</address>
+</footer>
+</body>
+</html>
---
+++ QuemSomos/quemsomos.php
@@ -0,0 +1,38 @@
+<?php
+include_once "head.php";
+?>
+ <!--<nav>
+        <img src="" alt="">
+
+        <ul>
+            <li>
+
+            </li>
+            <li>
+
+            </li>
+        </ul>
+    </nav>
+    <header>
+        <img src="" alt="">-->
+        <h1>TechLimp</h1>
+        <h2>Quem somos</h2>
+    </header>
+    <main>
+        <h2>
+            Quem somos
+        </h2>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda tempore recusandae fugiat distinctio odio ipsum maxime quo ut eaque dolorem accusantium atque culpa repudiandae esse minima odit commodi, dicta obcaecati.
+        </p>
+
+        <h2>
+            Qual a nossa proposta
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum voluptatem consequatur amet ratione. Obcaecati voluptates nihil porro facere aspernatur praesentium at voluptate, assumenda quo reprehenderit autem doloribus sequi libero distinctio!
+        </p>
+    </main>
+<?php
+ include_once "footer.php";
+?>
