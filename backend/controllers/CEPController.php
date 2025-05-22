7c189d91799b444aa7d3b7a3634b3d004c32f499
Caetano Thiago da Silva Ferreira <162740566+ThigasTT@users.noreply.github.com>
2025-05-22 14:46:46 -0300

    Update CEPController.php

--- backend/controllers/CEPController.php
+++ backend/controllers/CEPController.php
@@ -7,7 +7,7 @@ class CEPController {
     private $db;
     private $cepModel;
 
-    // Construtor ajustado para receber a conexão com o banco de dados
+
     public function __construct($dbConnection) {
         $this->db = $dbConnection;
         $this->cepModel = new CEP($this->db);
@@ -25,16 +25,16 @@ class CEPController {
     }
 
     public function salvarCep() {
-        // Lê os dados da requisição
+
         $data = json_decode(file_get_contents('php://input'), true);
 
-        // Valida se o campo 'cep' foi enviado
+
         if (!isset($data['cep'])) {
             echo json_encode(["error" => "O campo 'cep' é obrigatório."]);
             return;
         }
 
-        // Busca os dados do CEP
+  
         $dados = ViaCEPService::buscarCep($data['cep']);
 
         if (isset($dados['error'])) {
@@ -42,14 +42,14 @@ class CEPController {
             return;
         }
 
-        // Preenche o modelo com os dados do CEP
+     
         $this->cepModel->cep = $dados['cep'];
         $this->cepModel->logradouro = $dados['logradouro'];
         $this->cepModel->bairro = $dados['bairro'];
         $this->cepModel->cidade = $dados['localidade'];
         $this->cepModel->uf = $dados['uf'];
 
-        // Salva o CEP no banco de dados
+      
         if ($this->cepModel->save()) {
             echo json_encode(["message" => "CEP salvo com sucesso"]);
         } else {
@@ -57,9 +57,9 @@ class CEPController {
         }
     }
     
-//função que busva
-    public function buscarCEPs() {
-        $stmt = $this->cepModel->getAll();
+
+    public function buscarCEPs($cep) {
+        $stmt = $this->cepModel->getByCep($cep);
         $num = $stmt->rowCount();
     
         if ($num > 0) {
