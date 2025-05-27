9ea509fde9456a473dd703e51bafe398ca41baa8
Caetano Thiago da Silva Ferreira <162740566+ThigasTT@users.noreply.github.com>
2025-05-27 15:20:30 -0300

    Update cepService.ts

--- tlc_TCC/services/cepService.ts
+++ tlc_TCC/services/cepService.ts
@@ -1,6 +1,4 @@
 import api from "./api";
 
-export const getPoints = () => api.get("?url=ceps");
-export const createPoints = (data: any) => api.post("?url=ceps", data);
-export const updatePoints = (data: any) => api.put("?url=ceps", data);
-export const deletePoints = (data: any) => api.delete("?url=ceps", { data });
+export const getCeps = (cep: any) => api.get("?url=ceps", { params: { cep } });
+export const createCeps = (cep: any) => api.post("?url=ceps",cep);
